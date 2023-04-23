8eae686a121175bd49b80c4baa9d8f3a920149d8
Anton Slastuhin <antonslastuhin@gmail.com>
2023-04-23 19:14:16 +0300

    тестовая версия КнутМорисПрата

---
+++ SubstringSearchAlgorithms/Class/KnutMorisPrat/KnutMorisPrat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstringSearchAlgorithms.Class.KnutMorisPrat
+{
+    public class KnutMorisPrat : ISustringSearcher
+    {
+        public int[] IndexOf(string str, int startIndex, string substring)
+        {
+            var result = new List<int>();
+            var prefFunc = PrefixFunction<char>.BuildPrefixFunction(str.ToArray());
+
+            var len = str.Length;
+            var subLen = substring.Length;
+
+            var currLen = 0;
+            for(var i = startIndex; i < len; i++)
+            {
+                if(str[i] == substring[currLen])
+                {
+                    currLen++;
+
+                    //вхождение найдено
+                    if (currLen == subLen)
+                    {
+                        var currIndex = i - subLen + 1;
+                        result.Add(currIndex);
+                    }
+                }
+                else
+                {
+                    while(currLen > 0)
+                    {
+                        currLen = prefFunc[currLen - 1];
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ SubstringSearchAlgorithms/Class/KnutMorisPrat/PrefixFunction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstringSearchAlgorithms.Class.KnutMorisPrat
+{
+    public static class PrefixFunction<T>
+    {
+        public static int[] BuildPrefixFunction(T[] items,Comparer<T> comparer = null)
+        {
+            if (comparer == null) comparer = Comparer<T>.Default;
+            var len = items.Length;
+            var result = new int[len];
+
+            for(var i = 1; i < len; i++)
+            {
+                var suffixLen = result[i - 1];
+                var flag = comparer.Compare(items[i], items[suffixLen]) != 0;
+                while (suffixLen > 0 && flag)
+                {
+                    suffixLen = result[suffixLen - 1];
+                    flag = comparer.Compare(items[i], items[suffixLen]) != 0;
+                }
+                if (flag)
+                {
+                    suffixLen++;
+                }
+                result[i] = suffixLen;
+            }
+
+            return result;
+        }
+    }
+}
