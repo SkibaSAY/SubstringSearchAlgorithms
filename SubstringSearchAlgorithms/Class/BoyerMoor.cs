ea3f833225fa4f60067a420991a1873092249305
pr0gramm-ist <pr0gramm-ist@yandex.ru>
2023-04-23 16:04:55 +0300

    сделала заполнение таблиц для бойера мура

--- SubstringSearchAlgorithms/Class/BoyerMoor.cs
+++ SubstringSearchAlgorithms/Class/BoyerMoor.cs
@@ -6,25 +6,32 @@ using System.Threading.Tasks;
 
 namespace SubstringSearchAlgorithms.Class
 {
-    class BoyerMoor
+    public class BoyerMoor: ISustringSearcher
     {
         private Dictionary<char, int> stopSymbolsTable = new Dictionary<char, int>();
         private Dictionary<string, int> suffixTable = new Dictionary<string, int>();
 
+        public int[] IndexOf(string str, int startIndex, string substring)
+        {
+            FillStopSymbols(substring);
+            FillSuffixTable(substring);
+            return new int[0];
+        }
+
         private void FillStopSymbols(string pattern)
         {
             //тут нет ошибки. Последний символ не берем
-            for (int i = 0; i < pattern.Length - 1; i++)
+            for (int i = pattern.Length - 2; i >=0; i--)
             {
                 var currentSymbol = pattern[i];
-                if (stopSymbolsTable.ContainsKey(currentSymbol))
+                if (!stopSymbolsTable.ContainsKey(currentSymbol))
                 {
-                    stopSymbolsTable[currentSymbol] = i + 1;
+                    stopSymbolsTable.Add(currentSymbol, i);
                 }
-                else
-                {
-                    stopSymbolsTable.Add(currentSymbol, i + 1);
-                }
+            }
+            if (!stopSymbolsTable.ContainsKey(pattern[pattern.Length-1]))
+            {
+                stopSymbolsTable.Add(pattern[pattern.Length - 1], pattern.Length);
             }
         }
 
@@ -32,11 +39,44 @@ namespace SubstringSearchAlgorithms.Class
         {
             var suffix = "";
             suffixTable.Add(suffix, 1);
-            suffix += pattern[pattern.Length - 1];
+            var patternLength = pattern.Length;
+  
+            var trimPattern = pattern;
+
             int i = 1;
             while (suffix != pattern)
             {
-
+                suffix = pattern[patternLength - i] + suffix;
+                trimPattern = trimPattern.Remove(trimPattern.Length-1);
+                if (!trimPattern.Contains(suffix))
+                {
+                    var predSuffix = suffix.Substring(1);
+                    if (!trimPattern.Contains(predSuffix))
+                    {
+                        suffixTable.Add(suffix, suffixTable[predSuffix]);
+                    }
+                    else
+                    {
+                        var littleString = trimPattern;
+                        while (littleString!="")
+                        {
+                            if (suffix.EndsWith(littleString)) break;
+                            littleString = littleString.Remove(littleString.Length - 1);
+                        }
+                        suffixTable.Add(suffix, patternLength - littleString.Length);
+                    }
+                }
+                else
+                {
+                    var littleString = trimPattern.Substring(trimPattern.Length - suffix.Length);
+                    for(int j = trimPattern.Length - suffix.Length - 1; j >= 0; j--)
+                    {
+                        if (littleString.StartsWith(suffix)) break;
+                        littleString = trimPattern[j] + littleString;
+                    }
+                    suffixTable.Add(suffix, littleString.Length);
+                }
+                i++;
             }
         }
     }
