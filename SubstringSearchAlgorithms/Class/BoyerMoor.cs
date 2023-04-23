5866ad3556545949115e34167021fb07d4703618
pr0gramm-ist <pr0gramm-ist@yandex.ru>
2023-04-23 14:40:35 +0300

    пробую залить начало бойера мура

---
+++ SubstringSearchAlgorithms/Class/BoyerMoor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstringSearchAlgorithms.Class
+{
+    class BoyerMoor
+    {
+        private Dictionary<char, int> stopSymbolsTable = new Dictionary<char, int>();
+        private Dictionary<string, int> suffixTable = new Dictionary<string, int>();
+
+        private void FillStopSymbols(string pattern)
+        {
+            //тут нет ошибки. Последний символ не берем
+            for (int i = 0; i < pattern.Length - 1; i++)
+            {
+                var currentSymbol = pattern[i];
+                if (stopSymbolsTable.ContainsKey(currentSymbol))
+                {
+                    stopSymbolsTable[currentSymbol] = i + 1;
+                }
+                else
+                {
+                    stopSymbolsTable.Add(currentSymbol, i + 1);
+                }
+            }
+        }
+
+        private void FillSuffixTable(string pattern)
+        {
+            var suffix = "";
+            suffixTable.Add(suffix, 1);
+            suffix += pattern[pattern.Length - 1];
+            int i = 1;
+            while (suffix != pattern)
+            {
+
+            }
+        }
+    }
+}
